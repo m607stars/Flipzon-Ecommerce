6277479c767c1079f4d568f8962feb152d975cf1
Aafiya <aafiya.hussain2@gmail.com>
2021-02-24 00:49:23 +0530

    backend initialized

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/api/products',(req,res)=>{
+    res.send(data.products);
+})
+
+app.get('/',(req,res)=>{
+    res.send('Server is ready');
+});
+
+const port = process.env.port || 5000;
+app.listen(port, ()=>{
+    console.log(`Serve at port ${port}`);
+});
