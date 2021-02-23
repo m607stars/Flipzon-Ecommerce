b658c0f39bac83c76016181e8476a18b0e222bea
Talha Chafekar <talha1503@gmail.com>
2021-02-23 15:48:51 +0530

    added product and ratings component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Product from './components/Product';
 import data from './data';
+
 function App() {
   return (
     <div className="grid-container">
       <header className="row">
         <div>
           <a className="brand" href="/">
-            amazona
+            Flipzon
           </a>
         </div>
         <div>
@@ -18,39 +20,7 @@ function App() {
         <div>
           <div className="row center">
             {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
-            ))}
+              <Product key={product._id} product = {product}></Product>))}
           </div>
         </div>
       </main>
