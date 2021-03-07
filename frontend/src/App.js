282942ccebb4b8837b48be9b46ba9a5a90c37b0c
m607stars <mayankchowdhary607@gmail.com>
2021-03-07 12:54:57 +0530

    Implemented Add to Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,32 @@
+import { useSelector } from 'react-redux';
 import React from 'react';
-import { BrowserRouter,Route } from 'react-router-dom';
+import { BrowserRouter,Link,Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
+
+  const cart = useSelector(state => state.cart);
+  const {cartItems} = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               Flipzon
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className = "badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Rating from './Rating';
+import {Link} from 'react-router-dom';
 
 export default function Product(props) {
     const {product} = props;
@@ -12,17 +13,17 @@ export default function Product(props) {
 
     return (
         <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                   <img
                     className="medium"
                     src={product.image}
                     alt={product.name}
                   />
-                </a>
+                </Link>
                 <div className="card-body">
-                  <a href={`/product/${product._id}`}>
+                  <Link to={`/product/${product._id}`}>
                     <h2>{product.name}</h2>
-                  </a>
+                  </Link>
                 <Rating rating={product.rating} numReviews={product.numReviews} ></Rating>
                   <div className="price">${product.price}</div>
                 </div>
