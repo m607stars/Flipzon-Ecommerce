0e5ded71481495959abb89475b7d7b6d02351560
Talha Chafekar <talha1503@gmail.com>
2021-04-05 14:28:04 +0530

    sign in added

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,14 +1,39 @@
 import express from 'express';
-import User from '../models/userModel.js';
-import data from '../data.js'
 import expressAsyncHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
+import data from '../data.js';
+import User from '../models/userModel.js';
+import { generateToken } from '../utils.js';
 
 const userRouter = express.Router();
 
-userRouter.get('/seed', expressAsyncHandler(async (req,res)=>{
-    await User.remove({});
+userRouter.get(
+  '/seed',
+  expressAsyncHandler(async (req, res) => {
+    // await User.remove({});
     const createdUsers = await User.insertMany(data.users);
-    res.send({createdUsers});
-}));
+    res.send({ createdUsers });
+  })
+);
+
+userRouter.post(
+  '/signin',
+  expressAsyncHandler(async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: 'Invalid email or password' });
+  })
+);
 
 export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,14 @@ import data from './data.js';
 import mongoose from 'mongoose';
 import mongoURI from './keys.js';
 import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
+import dotenv from 'dotenv';
 
 const app = express();
 
-
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 const db = mongoURI;
 
 mongoose.connect(db,{useUnifiedTopology:true,useNewUrlParser:true}).then(()=>{
@@ -16,34 +20,17 @@ mongoose.connect(db,{useUnifiedTopology:true,useNewUrlParser:true}).then(()=>{
             console.log("Error has occurred while connecting to the database: ",err);
         })
 
-app.get('/api/products/:id',(req,res)=>{
-    console.log('server.js');
-    const product = data.products.find((x) => x._id === req.params.id);
-    if (product){
-        res.send(product);
-    } else {
-        res.status(404).send({message: 'Product not found' });
-    }
-});
-
-app.get('/api/products',(req,res)=>{
-    console.log('server.js');
-    console.log('sending');
-    console.log(data.products);
-    res.send(data.products);
-});
-
-app.use('/api/users/',userRouter);
-
-app.get('/',(req,res)=>{
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-app.use((err,req,res,next) =>{
-    res.status(500).send({message:err.message});
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
 });
 
-const port = process.env.port || 5000;
-app.listen(port, ()=>{
-    console.log(`Serve at port ${port}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
 });
