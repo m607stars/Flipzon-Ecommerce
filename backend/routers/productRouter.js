1be17669f91e0dc14639b337010c4b932b5e8046
Talha Chafekar <talha1503@gmail.com>
2021-04-15 01:56:44 +0530

    Deleting items on Admin side done

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -77,4 +77,21 @@ productRouter.put(
     }
   })
 );
+
+productRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async(req,res) =>{
+    const product = await Product.findById(req.params.id);
+    if(product){
+      const deletedProduct = await product.remove();
+      res.send({message: 'Product Deleted',product:deletedProduct})
+    }
+    else{
+      res.status(404).send({message:'Product not found'});
+    }
+  })
+);
+
 export default productRouter;
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -13,6 +13,10 @@ const {
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAIL,
   PRODUCT_UPDATE_RESET,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_RESET,
 } = require('../constants/productConstants');
 
 export const productListReducer = (
@@ -71,3 +75,18 @@ export const productUpdateReducer = (state = {}, action) => {
       return state;
   }
 };
+
+export const productDeleteReducer = (state = {}, action) => {
+  switch(action.type){
+    case PRODUCT_DELETE_SUCCESS:
+      return {loading:false,success:true};
+    case PRODUCT_DELETE_REQUEST:  
+      return {loading:true};
+    case PRODUCT_DELETE_FAIL:
+      return {loading:false,error:action.payload};
+    case PRODUCT_DELETE_RESET:
+      return {};
+    default:
+      return state;
+  }
+}
